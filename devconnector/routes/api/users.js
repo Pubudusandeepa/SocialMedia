fe3ab345a434d9d98ee99bd4e135f2633d490a49
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-03 12:04:19 +0530

    create user registration part1

--- devconnector/routes/api/users.js
+++ devconnector/routes/api/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router =  express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-//@route Get api/users
+//@route Post api/users
 //@desc Test route
 //@access Public
 
-router.get('/', (req, res) => res.send('Posts route'));
+router.post('/',
+[
+  check('name', 'Name is Required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'please enter the password with 6 or more characters').isLength({ min: 6})
+], 
+(req, res) => {
+   const errors = validationResult(req);
+   if(!errors.isEmpty()){
+       return res.status(400).json({errors: errors.array()});
+   }
+    res.send('Posts route');
+});
 module.exports = router;
