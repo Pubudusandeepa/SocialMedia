93981e4d028592d2bba695f9ddd9e4cd86760444
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-04 17:27:59 +0530

    rebuilding

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -12,7 +12,7 @@ const User = require('../../models/User');
 router.get('/me', auth, async  (req, res) =>{
 
      try{
-        const profile = await (await Profile.findOne({ user: req.user.id })).populate('user',
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user',
         ['name', 'avatar']);
 
         if(!profile){
