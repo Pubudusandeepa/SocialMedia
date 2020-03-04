058cb1cc012ea1ba0e2249229b19611e91d0fbfa
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-04 16:50:01 +0530

    Add middleware

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router =  express.Router();
+const auth = require('../../middleware/auth');
 
-//@route Get api/profile
+const Profile = require('../../models/Prifile');
+const User = require('../../models/User');
+
+//@route Get api/profile/me
 //@desc Test route
 //@access Public
 
-router.get('/', (req, res) => res.send('Posts route'));
+router.get('/me', auth, async  (req, res) =>{
+
+     try{
+        const profile = await (await Profile.findOne({ user: req.user.id })).populate('user',
+        ['name', 'avatar']);
+
+        if(!profile){
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile);
+     }catch(err){
+       console.error(err.message);
+       res.status(500).send('Server Error');
+     }
+
+    res.send('Posts route')
+} );
 module.exports = router;
