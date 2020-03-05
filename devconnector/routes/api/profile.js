2a1f5f4406873ab3429bf963a87a0d31188d8558
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-05 11:01:38 +0530

    Get all profile

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -31,7 +31,7 @@ router.get('/me', auth, async  (req, res) =>{
 
 //@route Get api/profile/me
 //@desc Test route
-//@access Public
+//@access Private
 
 router.post('/', 
 [auth,
@@ -112,6 +112,24 @@ router.post('/',
    console.log(profileFields.skills);
   //  res.send('Hello');
 
+});
+
+//@route Get api/profile/me
+//@desc Test route
+//@access Public
+
+router.get('/', async(req, res) =>{
+    try {
+       const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+       res.json(profiles);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+
+    }
+
 })
+
+
 
 module.exports = router;
