82a76e69fee1f8f9935cf67754cf00730cbe0edc
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-05 11:48:29 +0530

    build function if  user has not a profile

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -120,10 +120,17 @@ router.post('/',
 
 router.get('/user/:user_id', async(req, res) =>{
     try {
-       const profiles = await Profile.find().populate('user', ['name', 'avatar']);
-       res.json(profiles);
+       const profile = await Profile.findOne({
+         user: req.params.user_id
+       }).populate('user', ['name', 'avatar']);
+
+       if(!profile) return res.status(400).json({ msg: 'profile is not found'});
+       res.json(profile);
     } catch (error) {
       console.error(error.message);
+      if(error.kind == 'ObjectId') {
+        return res.status(400).json({ msg: 'Profile not found'});
+      }
       res.status(500).send('Server Error');
 
     }
