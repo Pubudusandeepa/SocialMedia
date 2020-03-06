b17b75764d121f26da2540bceebba1db1c925922
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-06 17:14:42 +0530

    post like route

--- devconnector/routes/api/posts.js
+++ devconnector/routes/api/posts.js
@@ -103,4 +103,33 @@ router.delete('/:id', auth, async (req, res) =>{
         
     }
 });
+ 
+//@route Put api/posts/like/:id
+//dest  Like a post
+//access private
+
+router.put('/like/:id', auth, async (req, res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //check if the post has alrady been liked
+           if(
+               post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+           ) {
+               return res.status(400).json({ msg: 'Post already liked '});
+           }
+
+           post.likes.unshift({ user: req.user.id });
+
+           await post.save();
+
+           res.json(post.likes);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+
 module.exports = router;
