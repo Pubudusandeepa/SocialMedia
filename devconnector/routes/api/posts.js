bb17278afb8d29784ed423f9fcb1af0702442c6a
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-06 12:42:33 +0530

    re build and change https

--- devconnector/routes/api/posts.js
+++ devconnector/routes/api/posts.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router =  express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-//@route Get api/posts
-//@desc Test route
-//@access Public
+const Post = require('../../models/Post');
+const Profile = require('../../models/Prifile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('Posts route'));
+//@route Post api/posts
+//@desc Create a post
+//@access private
+
+router.post('/', 
+[auth,
+     [
+   check('text', 'Text is required').not().isEmpty()
+]], 
+async (req, res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()});
+    }
+    const user = await User.findById(req.user.id).select('-password');
+    
+    try {
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        const post = await newPost.save();
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }  
+});
 module.exports = router;
