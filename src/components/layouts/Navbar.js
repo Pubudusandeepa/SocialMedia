4ba44fb464cb0c377e9b7d0b60238ed6aa73d2c6
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-07 22:58:39 +0530

    Add register and login form

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
         <h1>
-          <Link to='/'><i class="fas fa-code"></i> DevConnector</Link>
+          <Link to='/'><i className="fas fa-code"></i> DevConnector</Link>
         </h1>
         <ul>
           <li><a href="#3"></a>Developers</li>
