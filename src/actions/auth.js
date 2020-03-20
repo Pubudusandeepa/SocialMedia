63bc64a88b312841ab3d6013389e2e1b92e42639
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-20 19:48:56 +0530

    clear profile

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,7 +7,8 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../uitils/setAuthToken';
 
@@ -107,7 +108,13 @@ export const login = (body) => async dispatch => {
 
 export const logout = () => dispatch => {
     dispatch({
-        type: 'LOGOUT'
-    })
+        type: CLEAR_PROFILE,
+    
+    });
+
+    dispatch({
+        type: LOGOUT,
+    
+    });
 }
 
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layouts/Spinner';
 import {getCurrentProfile } from '../../actions/profile';
+import { Link } from 'react-router-dom';
 
 const Dashboard = ({ getCurrentProfile, auth:{user}, profile: { profile, loading }}) => {
      
@@ -15,6 +16,13 @@ const Dashboard = ({ getCurrentProfile, auth:{user}, profile: { profile, loading
         <p className="lead">
           <i className="fas fa-user"></i> welcome { user && user.name}  
         </p>
+        {profile !== null ? (<Fragment>has</Fragment>) 
+        : (<Fragment>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to='/create-profile' className='btn btn-primary my-1'>
+                Create Profile
+            </Link>
+        </Fragment>)}
         </Fragment>
 };
 
